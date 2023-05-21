7c335f87ec4935e8991cc6a4fbcdbc1dbfc97050
Tushar <tusharsharma123456.k20@gmail.com>
2023-05-21 21:30:47 +0530

    added one more project

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,16 +1,23 @@
-import Proj1 from "../assets/proj1.jpg";
-import Proj2 from "../assets/proj2.jpg";
+import proj1 from "../assets/proj1.jpg";
+import proj2 from "../assets/proj2.jpg";
+import proj3 from "../assets/proj3.jpg";
+
 
 export const ProjectList = [
   {
     name: "Weather App",
-    image: Proj1,
+    image: proj1,
     skills: "JavaScript,HTML,CSS,React.js",
   },
   {
     name: "React Movie Info App",
-    image: Proj2,
+    image: proj2,
     skills: "React.js,HTML,CSS,JavaScript,OMDB Public API",
+  },
+  {
+    name:"Personal Portfolio",
+    image:proj3,
+    skills:"React.js,React-router-dom,HTML,CSS,JavaScript,Material-ui v5"
   }
   
 ];
